073af740421a4fa20cb050b35623efcafc8cd7a9
perozdero <perozdero111@hi.t-com.hr>
2018-03-06 10:16:16 +0100

    wrong import

--- src/demo/demo.component.ts
+++ src/demo/demo.component.ts
@@ -1,7 +1,6 @@
 import { Observable } from 'rxjs/Observable';
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Options } from 'selenium-webdriver/opera';
 import { testDataFallBack } from './testdata';
 @Component({
   selector: 'pzd-demo',
